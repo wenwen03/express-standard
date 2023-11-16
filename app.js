e061ce2d3efb4a4de0452d0835e928f6a88920c9
furuta03 <wewen915@gmail.com>
2023-11-16 15:04:55 +0900

    feat(app.js): setup the access logging configuration.

--- app.js
+++ app.js
@@ -3,13 +3,32 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const fs = require('fs');
+const rfs = require('rotating-file-stream');
+const moment = require('moment-timezone');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
-app.use(logger('dev'));
+//ログの設定
+app.use(logger('short'));
+const logDirectory = path.join(__dirname, './logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+const accessLogStream = rfs.createStream('access.log', {
+  size:'10MB',
+  interval: '1d',
+  compress: 'gzip',
+  path: logDirectory,
+  rotate: 30
+});
+logger.token('date', function (req, res) {
+  return moment().tz("Asia/Tokyo").format('YYYY-MM-DD HH:mm:ss');
+});
+app.use(logger(':date[clf] ":method :url HTTP/:http-version" :status :res[content-length] - :response-time ms'));
+app.use(logger('combined',{stream: accessLogStream}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
