4673c7ccfba6d84a2cabe63564863060cf5e02d7
furuta03 <wewen915@gmail.com>
2023-11-20 17:20:43 +0900

    feat(notFoundHandler): create a notFoundHandler

---
+++ src/middlewares/notFoundHandler.ts
@@ -0,0 +1,8 @@
+import type { Request, Response, NextFunction } from 'express';
+import createError from 'http-errors';
+
+export default function notFoundHandler(_: Request, res: Response, next: NextFunction) {
+  if(!res.headersSent) {
+    next(createError(404));
+  }
+}
