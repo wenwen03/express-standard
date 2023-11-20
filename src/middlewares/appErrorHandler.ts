80dfd4200e07639c348b42206df88c33e0554d4f
furuta03 <wewen915@gmail.com>
2023-11-20 17:20:11 +0900

    feat(appErrorHandler): create an appErrorHandler

---
+++ src/middlewares/appErrorHandler.ts
@@ -0,0 +1,29 @@
+import type { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import * as rfs from 'rotating-file-stream';
+import moment from 'moment-timezone';
+
+const logDirectory = path.join(__dirname, '../../logs/error');
+const timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
+const logStream = rfs.createStream('error.log', {
+  size: '10MB',
+  interval: '1d',
+  path: logDirectory,
+  compress: 'gzip',
+  rotate: 30
+});
+
+export default function appErrorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  const errorLogContent = `${timestamp} - ${req.method} ${req.url} - ${err.message}\n`;
+  logStream.write(errorLogContent);
+
+  res.status(err.status || 500).json({
+    status: 'error',
+    message: err.message,
+  });
+}
+
